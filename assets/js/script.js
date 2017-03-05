fde3fb13ce609d04e2675865b232652ca2cefaf2
jpetey <jeremee.peters@gmail.com>
2017-03-05 09:30:08 -0800

    JS updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-
-
 // This function handles events when SEARCH button is clicked
   $("#submit-data").on("click", function(event) {
     
@@ -19,6 +17,72 @@
     var endYear = $("#end-year").val().trim();
     console.log(endYear);
 
-    // Adding movie from the textbox to our array
-    // movies.push(movie);
-});    
+    // This is my API key
+    var apiKey = "77052775b2b34705b38c13c6e71667d7";
+
+  //Re-render #search-results to display the appropriate content
+
+    // Define URL
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + apiKey 
+                    + "&q=" + searchTerms
+                    + "&page=1&begin_date=" + startYear + "0000"
+                    + "&end_date=" + endYear + "0000";
+    
+    // Creating an AJAX call for the specific movie button being clicked
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).done(function(response) {
+      
+      // Create a div to hold each group of search results
+      var searchResults = $("<div class='news-articles'>");
+      
+      // Store the headline data
+      var headline = response.docs.headline.main;
+
+      // Create an element to have the Headline displayed
+      var pHeadline = $("<p>").text(headline);
+
+      // Displaying the Headline
+      searchResults.append(pHeadline);
+
+      // Store the author data
+      var author = response.docs.byline.original;
+
+      // Create an element to have the author displayed
+      var pAuthor = $("<p>").text(author);
+
+      // Displaying the author
+      searchResults.append(pAuthor);
+
+      // Store the section data
+      var section = response.docs.section_name;
+
+      // Create an element to have the section name displayed
+      var pSection = $("<p>").text("Section: " + section);
+
+      // Displaying the section
+      searchResults.append(pSection);
+
+      // Store the date-released data
+      var dateReleased = response.docs.pub_date;
+
+      // Create an element to have the date released displayed
+      var pDateReleased = $("<p>").text(dateReleased);
+
+      // Displaying the date released
+      searchResults.append(pDateReleased);
+
+      // Store the Link to the News Story
+      var linkToContent = response.docs.web_url;
+
+      // Creating an element to hold the Link to the News Story
+      var aLinkToContent = $("<a>").attr("href", linkToContent);
+
+      // Append the Link to the News Story
+      searchResults.append(aLinkToContent);
+
+      // Put each block of articles on the page
+      $("#search-results").append(searchResults);
+    });
+  });
